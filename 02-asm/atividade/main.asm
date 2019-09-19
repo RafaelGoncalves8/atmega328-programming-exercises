844dc3c485818ea951204e965ae9867b2198d935
Rafael Goncalves <rafaelgoncalves258@gmail.com>
2019-09-19 20:49:35 -0300

    Implemented blink.

--- 02-asm/atividade/main.asm
+++ 02-asm/atividade/main.asm
@@ -1,61 +1,117 @@
 .device ATmega328P
 
-.org 0x0000     ; program starts at 0x0000 (value of PC after reset)
+; Define registers
+.def rCounter  = r16
+.def rCounterIn = r25
+; .def rCounterInIn = r26
+.def rAux = r24
+
+; Define constants
+.equ cInner = 0xFF
+.equ cOuter = 0xFF
+; .equ cInIn  = 0x0F
+
+; program starts at 0x0000 (value of PC after reset)
+.org 0x0000
 jmp main
 
-.org 0x0034     ; first address after reserved portion for interruptions and vectors
+; first address after reserved portion for interruptions and vectors
+.org 0x0034
 main:
 
 ; configures pin 13 as output
-lds r24,0x24
-ori r24,0b00100000
-sts 0x24,r24
+lds rAux,0x24
+ori rAux,0b00100000
+sts 0x24,rAux
 
 ; configures pin A0 as input
-lds r24,0x27
-andi r24,0b1111110
-sts 0x27,r24
+lds rAux,0x27
+andi rAux,0b1111110
+sts 0x27,rAux
 
 ; configures internal pullup resistor
-lds r24,0x26
-ori r24,0b00000001
-sts 0x26,r24
+lds rAux,0x26
+ori rAux,0b00000001
+sts 0x26,rAux
 
-; turn off led initially
-lds r24,0x25
-andi r24,0b11011111
-sts 0x25,r24
+main_loop:
+; lds r16,0x26    ; read button
+; andi r16,0x01   ; apply mask
+; brbs 1, fast    ; branch if button on
 
 loop:
 
-; lds r16,0x26    ; read button
-; andi r16,0x01  ; apply mask
-; brbs 1, fast  ; branch if button on
+; Led off
+lds rAux,0x25            ; 2 clock cycle
+andi rAux,0b11011111     ; 1 clock cycle
+sts 0x25,rAux            ; 2 clock cycle
 
-;set led on
-lds r24,0x25
-ori r24,0b00100000
-sts 0x25,r24
+ldi rCounter, cOuter    ; 1 clock cycle
 
-;set led off
-lds r24,0x25
-andi r24,0b11011111
-sts 0x25,r24
+loop1:
+ldi rCounterIn, cInner  ; 1 clock cycle
 
-rjmp loop
+loop1i:
+dec rCounterIn          ; 1 clock cycle
+nop                     ; 1 clock cycle
+nop                     ; 1 clock cycle
+nop                     ; 1 clock cycle
+nop                     ; 1 clock cycle
+nop                     ; 1 clock cycle
+nop                     ; 1 clock cycle
+nop                     ; 1 clock cycle
+nop                     ; 1 clock cycle
+nop                     ; 1 clock cycle
+nop                     ; 1 clock cycle
+brne loop1i             ; if not zero: jmp(2 cycle), else: 1cycle
+dec rCounter            ; 1 clock cycle
+brne loop1              ; if not zero: jmp(2 cycle), else: 1cycle
+nop                     ; 1 clock cycle
 
-led_off:
+set led on
+lds rAux,0x25            ; 2 clock cycle
+ori rAux,0b00100000      ; 1 clock cycle
+sts 0x25,rAux            ; 2 clock cycle
 
-lds r24,0x25
-andi r24,0b11011111
-sts 0x25,r24
+ldi rCounter, cOuter    ; 1 clock cycle
 
-rjmp loop
+loop2:
+ldi rcounterIn, cInner  ; 1 clock cycle
 
-led_on:
+loop2i:
+dec rCounterIn          ; 1 clock cycle
+brne loop2i             ; if not zero: jmp(2 cycle), else: 1cycle
+dec rCounter            ; 1 clock cycle
+brne loop2              ; if not zero: jmp(2 cycle), else: 1cycle
+nop                     ; 1 clock cycle
 
-lds r24,0x25
-ori r24,0b00100000
-sts 0x25,r24
+rjmp loop               ; 2 clock cycle
 
-rjmp loop
+
+; ;set led on
+; lds rAux,0x25            ; 2 clock cycle
+; ori rAux,0b00100000      ; 1 clock cycle
+; sts 0x25,rAux            ; 2 clock cycle
+;
+; ;set led off
+; lds rAux,0x25            ; 2 clock cycle
+; andi rAux,0b11011111     ; 1 clock cycle
+; sts 0x25,rAux            ; 2 clock cycle
+;
+; rjmp loop
+;
+; led_off:
+;
+; lds rAux,0x25            ; 2 clock cycle
+; andi rAux,0b11011111     ; 1 clock cycle
+; sts 0x25,rAux            ; 2 clock cycle
+;
+; rjmp loop
+;
+; led_on:
+;
+; lds rAux,0x25            ; 2 clock cycle
+; ori rAux,0b00100000      ; 1 clock cycle
+; sts 0x25,rAux            ; 2 clock cycle
+;
+; rjmp loop
